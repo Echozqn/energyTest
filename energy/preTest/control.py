1c55b2ce78e737f82c7ccb61014d528ed9d9ede5
echozqn <1870571113@qq.com>
2023-05-26 22:30:41 +0800

    test

--- energy/preTest/control.py
+++ energy/preTest/control.py
@@ -5,7 +5,7 @@ model_dataset = ['resnet50_cifar10','vgg19_cifar10']
 for file in model_dataset:
     while True:
         if small_batch < 32:break
-        file_name = f"{file}.log"
+        file_name = f"{file}_{batch}_{small_batch}.log"
         cmd = f"python3 {file}.py {batch} {small_batch} {file_name}"
         print(cmd)
         os.system(cmd)
