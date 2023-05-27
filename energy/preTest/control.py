0a280eb20905e1ea19a905920c8b779cee831143
echozqn <1870571113@qq.com>
2023-05-27 09:21:26 +0800

    test

--- energy/preTest/control.py
+++ energy/preTest/control.py
@@ -1,13 +1,14 @@
 import os
-model_dataset = ['resnet50_cifar10','vgg19_cifar10','BERT_SST2']
-batches = [512,512,32]
-small_batches = [32,32,4]
-for i in range(1,len(model_dataset)):
+
+model_dataset = ['resnet50_cifar10', 'vgg19_cifar10', 'BERT_SST2']
+batches = [512, 512, 32]
+small_batches = [32, 32, 4]
+for i in range(1, len(model_dataset)):
     file = model_dataset[i]
     batch = batches[i]
     small_batch = batch
     while True:
-        if small_batch < small_batches[i]:break
+        if small_batch < small_batches[i]: break
         file_name = f"{file}_{batch}_{small_batch}.log"
         cmd = f"python3 {file}.py {batch} {small_batch} {file_name} 5"
         print(cmd)
