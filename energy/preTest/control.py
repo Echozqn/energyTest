d9beab40ea2c260b00e5da33492e3164734fd04d
echozqn <1870571113@qq.com>
2023-05-27 09:45:40 +0800

    test

--- energy/preTest/control.py
+++ energy/preTest/control.py
@@ -5,16 +5,17 @@ import Constant
 publicFunction.remove(Constant.CSV_FILE_NAME)
 model_dataset = ['resnet50_cifar10', 'vgg19_cifar10', 'BERT_SST2']
 batches = [512, 512, 32]
-small_batches = [16, 16, 2]
+small_batches = [32, 32, 4]
 
 for i in range(0, len(model_dataset)):
-    file = model_dataset[i]
     batch = batches[i]
-    small_batch = batch
-    while True:
-        if small_batch < small_batches[i]: break
-        file_name = f"{file}_{batch}_{small_batch}.log"
-        cmd = f"python3 {file}.py {batch} {small_batch} {file_name} 5"
-        print(cmd)
-        os.system(cmd)
-        small_batch //= 2
+    while batch >= small_batches[i]:
+        file = model_dataset[i]
+        small_batch = batch
+        while small_batch >= small_batches[i]:
+            file_name = f"{file}_{batch}_{small_batch}.log"
+            cmd = f"python3 {file}.py {batch} {small_batch} {file_name} 3"
+            print(cmd)
+            os.system(cmd)
+            small_batch //= 2
+        batch //= 2
