f893859e6188730f375f959ecf7689dfab0943cf
echozqn <1870571113@qq.com>
2023-05-29 22:31:32 +0800

    test2

--- energy/preTest/controlLoss.py
+++ energy/preTest/controlLoss.py
@@ -2,12 +2,11 @@ import os
 import publicFunction
 import Constant
 
-for i in range(5):
-    file = 'resnet50_cifar10'
-    small_batch = 256
-    while small_batch >= 32:
-        file_name = f"{file}_{str(256)}_{str(small_batch)}_{i}_loss.log"
-        cmd = f"python3 {file}.py 256 {str(small_batch)} {file_name} 10"
-        print(cmd)
-        os.system(cmd)
-        small_batch //= 2
+file = 'resnet50_cifar10'
+small_batch = 256
+while small_batch >= 32:
+    file_name = f"{file}_{str(256)}_{str(small_batch)}_{i}_loss.log"
+    cmd = f"python3 {file}.py 256 {str(small_batch)} {file_name} 20"
+    print(cmd)
+    os.system(cmd)
+    small_batch //= 2
