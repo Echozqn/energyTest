8d15164b54bd52f67ffcbd8b775f97590dbf4b64
echozqn <1870571113@qq.com>
2023-05-29 13:23:48 +0800

    test2

--- energy/preTest/controlLoss.py
+++ energy/preTest/controlLoss.py
@@ -6,8 +6,8 @@ import Constant
 file = 'resnet50_cifar10'
 small_batch = 256
 while small_batch >= 32:
-    file_name = f"{file}_{256}_{small_batch}.log"
-    cmd = f"python3 {file}.py 256 {small_batch} {file_name} 15"
+    file_name = f"{file}_{str(256)}_{small_batch}.log"
+    cmd = f"python3 {file}.py 256 {str(small_batch)} {file_name} 15"
     print(cmd)
     os.system(cmd)
     small_batch //= 2
