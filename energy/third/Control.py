316e939ef142639de1c41246234bdc6d1913810f
echozqn <1870571113@qq.com>
2023-06-26 23:44:04 +0800

    test5

--- energy/third/Control.py
+++ energy/third/Control.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 from Common import Constant
 from Common import publicFunction
 
@@ -7,16 +9,20 @@ model_dataset = 'resnet50_cifar10'
 batch = 512
 publicFunction.remove(Constant.CSV_FILE_NAME)
 for fre in range(900, 1601,100):
+    lock_fre = f"nvidia-smi -lgc {fre}"
+    print(lock_fre)
+    os.system(lock_fre)
+
     dmon_name = f"./dmon/dmon_{fre}_{model_dataset}_{batch}_{batch}.log"
     dmon_cmd = f"nvidia-smi dmon -f {dmon_name} &"
     print(dmon_cmd)
-    os.system(dmon_cmd)
-
+    # os.system(dmon_cmd)
+    time.sleep(5)
     file_name = f"{fre}_{model_dataset}_{batch}_{batch}.log"
     cmd = f"python3 {model_dataset}.py {batch} {batch} {file_name} 3 {fre}"
-    os.system(cmd)
+    # os.system(cmd)
     print(cmd)
 
     kill_cmd = "pgrep -f \"nvidia-smi dmon -f\" | xargs kill"
     print(kill_cmd)
-    os.system(kill_cmd)
+    # os.system(kill_cmd)
